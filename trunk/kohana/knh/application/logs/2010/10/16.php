8c292b6c61b0892e576f0963f501ac13df60795c
aquilax <aquilax@872bc89c-b911-77d3-5228-32eba119024b>
2010-10-16 07:39:25 +0000

    Initial import

---
+++ trunk/kohana/knh/application/logs/2010/10/16.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2010-10-16 10:22:31 --- ERROR: ReflectionException [ -1 ]: Class controller_news does not exist ~ SYSPATH/classes/kohana/request.php [ 1094 ]
+2010-10-16 10:22:43 --- ERROR: ReflectionException [ -1 ]: Class controller_news does not exist ~ SYSPATH/classes/kohana/request.php [ 1094 ]
+2010-10-16 10:22:46 --- ERROR: ReflectionException [ -1 ]: Class controller_news does not exist ~ SYSPATH/classes/kohana/request.php [ 1094 ]
+2010-10-16 10:23:00 --- ERROR: ReflectionException [ -1 ]: Class controller_news does not exist ~ SYSPATH/classes/kohana/request.php [ 1094 ]
